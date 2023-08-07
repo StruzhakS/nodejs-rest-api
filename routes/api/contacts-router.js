431ec4a7176bcd038a756d085c2e54226ca3961a
StruzhakS <StruzhakS@gmail.com>
2023-08-07 11:33:02 +0300

    bugfixed at delete method

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -65,6 +65,9 @@ contactsRouter.delete('/:contactId', async (req, res, next) => {
     const { contactId } = req.params;
 
     const result = await Contact.findByIdAndRemove({ _id: contactId });
+    if (!result) {
+      return res.status(404).json({ message: `Movie with id=${contactId} not found` });
+    }
     res.json(result);
   } catch (error) {
     next(error);
